d2b755d18ce2dd6332a9cbd97660ba2f2fc7fca2
Aseel Cortbawi <aseel.cortbawi@gmail.com>
2025-07-11 13:44:15 -0400

    worked on Recipe page to created it , make sure form was working and be able to sumbit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import AddRecipe from "./layout/AddRecipe";
+import RecipeList from "./layout/RecipeList";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        <Route path="/recipe" element={<AddRecipe />} />
+        <Route path="/recipes" element={<RecipeList />} />
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
