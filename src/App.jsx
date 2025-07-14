ea0c2529c0489eae9f9830287ad242b7a1558aed
Brandon Corrigall <corrb812@gmail.com>
2025-07-14 11:57:41 -0400

    updated app.jsx with currently known routes to be used

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import HomePage from "./layout/Home.jsx";
+import AddRecipe from "./layout/AddRecipe";
+import RecipeList from "./layout/RecipeList";
+import Contact from "./layout/Contact";
+import AboutUs from "./layout/AboutUs";
+import Recipe from "./layout/Recipe";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<p>Home page</p>} />
+        <Route index element={<HomePage />} />
+        <Route path="/recipe" element={<Recipe />} />
+        <Route path="/addrecipe" element={<AddRecipe />} />
+        <Route path="/recipes" element={<RecipeList />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
       </Route>
     </Routes>
   );
